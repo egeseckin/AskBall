3d944fb17ce18d6e320999693dea77e000acfc14
egeseckin <ege.seckin@outlook.com>
2021-09-04 11:55:43 +0300

    Ask Ball app made

--- AskBall/ViewController.swift
+++ AskBall/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Ball: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Ball.image = #imageLiteral(resourceName: "ball5")
     }
 
-
+    @IBAction func askButton(_ sender: Any) {
+        Ball.image = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")][Int.random(in:0...4)]
+    }
+    
+    
 }
 
